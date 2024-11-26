85070d3d73159c125e57e999915dd52727b8aede
KubilayKilic <kilickubilaykaan@gmail.com>
2024-11-26 11:46:14 +0300

    hatalar giderildi

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,7 +2,7 @@ import CardWrapper from "@/app/ui/dashboard/cards";
 import RevenueChart from "../../ui/dashboard/revenue-chart";
 import LatestInvoices from "../../ui/dashboard/latest-invoices";
 import { lusitana } from "../../ui/fonts";
-import { fetchCardData } from "../../lib/data";
+//import { fetchCardData } from "../../lib/data";
 import { Suspense } from "react";
 import {
   LatestInvoicesSkeleton,
